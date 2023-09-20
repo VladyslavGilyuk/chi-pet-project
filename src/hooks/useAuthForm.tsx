4911ae5774ea5ec2ef08e3e5bca809862a67525b
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-09-21 00:54:37 +0300

    feat: db, auth, axious post method

--- src/hooks/useAuthForm.tsx
+++ src/hooks/useAuthForm.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { FieldValues, SubmitHandler } from 'react-hook-form';
 interface ISignIn {
@@ -29,6 +30,10 @@ const useAuthForm = () => {
   };
 
   const onSignUpSubmit: SubmitHandler<ISignUp> = (data: ISignUp) => {
+    axios
+      .post('http://localhost:8080/register', data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.message));
     console.log('Form data:', data);
   };
 
