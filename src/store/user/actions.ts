94f1fe48069fb794f32bd60336daf68d9e8784a8
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-09-26 16:39:58 +0300

    fix: await changed to then()

--- src/store/user/actions.ts
+++ src/store/user/actions.ts
@@ -6,44 +6,45 @@ import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 export const setUser = createAction<UserState>('user/setUser');
 
 export const signInAsync = createAsyncThunk('auth/signIn', async (data: ISignIn) => {
-  try {
-    const response = await UserService.login(data);
-    const responseData = response.data;
-    console.log(responseData);
-    if (responseData) {
-      localStorage.setItem(
-        'user',
-        JSON.stringify({
-          token: responseData.accessToken,
-          ...responseData.user,
-        }),
-      );
-    }
-
-    return responseData;
-  } catch (error) {
-    console.log(error);
-    alert('An error occurred. Please try again later.');
-  }
+  return UserService.login(data)
+    .then((response) => {
+      const responseData = response.data;
+      console.log(responseData);
+      if (responseData) {
+        localStorage.setItem(
+          'user',
+          JSON.stringify({
+            token: responseData.accessToken,
+            ...responseData.user,
+          }),
+        );
+      }
+      return responseData;
+    })
+    .catch((error) => {
+      console.log(error);
+      alert('An error occurred. Please try again later.');
+    });
 });
 
 export const signUpAsync = createAsyncThunk('auth/signUp', async (data: ISignUpAsync) => {
-  try {
-    const response = await UserService.register(data);
-    const responseData = response.data;
-    console.log(responseData);
-    if (responseData) {
-      localStorage.setItem(
-        'user',
-        JSON.stringify({
-          token: responseData.accessToken,
-          ...responseData.user,
-        }),
-      );
-    }
-    return responseData;
-  } catch (error) {
-    console.log(error);
-    alert('An error occurred. Please try again later.');
-  }
+  return UserService.register(data)
+    .then((response) => {
+      const responseData = response.data;
+      console.log(responseData);
+      if (responseData) {
+        localStorage.setItem(
+          'user',
+          JSON.stringify({
+            token: responseData.accessToken,
+            ...responseData.user,
+          }),
+        );
+      }
+      return responseData;
+    })
+    .catch((error) => {
+      console.log(error);
+      alert('An error occurred. Please try again later.');
+    });
 });
