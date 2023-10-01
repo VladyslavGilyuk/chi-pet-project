3084aea998ead1977f5d6f48f13d6c45c272816c
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-10-01 16:09:36 +0300

    fix: NavLink component

--- src/components/links/index.tsx
+++ src/components/links/index.tsx
@@ -1,30 +1,29 @@
 import { INavLinks } from '../navBar/helper';
+import { useLocation } from 'react-router-dom';
 import { LinksWrapper, StyledIconWrapper, StyledLink } from './styled';
 
 export interface LocationType {
   pathname: string;
 }
-const Links: React.FC<{ links: Array<INavLinks>; location: LocationType }> = ({
-  links,
-  location,
-}) => {
+interface NavLinkProps {
+  link: INavLinks;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ link }) => {
+  const location: LocationType = useLocation();
   return (
-    <>
-      {links.map((link) => (
-        <div key={link.name}>
-          {link.divider}
-          <LinksWrapper active={location.pathname === link.to ? 'true' : ''}>
-            <StyledIconWrapper active={location.pathname === link.to ? 'true' : ''}>
-              {link.icon}
-            </StyledIconWrapper>
-            <StyledLink to={link.to} active={location.pathname === link.to ? 'true' : ''}>
-              {link.name}
-            </StyledLink>
-          </LinksWrapper>
-        </div>
-      ))}
-    </>
+    <div>
+      {link.divider}
+      <LinksWrapper active={location.pathname === link.to ? 'true' : ''}>
+        <StyledIconWrapper active={location.pathname === link.to ? 'true' : ''}>
+          {link.icon}
+        </StyledIconWrapper>
+        <StyledLink to={link.to} active={location.pathname === link.to ? 'true' : ''}>
+          {link.name}
+        </StyledLink>
+      </LinksWrapper>
+    </div>
   );
 };
 
-export default Links;
+export default NavLink;
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,11 +1,9 @@
 import { ReactComponent as LogoIcon } from '../../assets/logo.svg';
+import NavLink from '../links';
 import { links } from './helper';
-import { useLocation } from 'react-router-dom';
 import { HeaderWrapper, HeadingText, NavBarContainer, StyledImage } from './styled';
-import Links, { LocationType } from '../links';
 
 const NavBar: React.FC = () => {
-  const location: LocationType = useLocation();
   return (
     <NavBarContainer>
       <HeaderWrapper>
@@ -14,7 +12,9 @@ const NavBar: React.FC = () => {
         </StyledImage>
         <HeadingText variant='h3'>Dashboard Kit</HeadingText>
       </HeaderWrapper>
-      <Links links={links} location={location} />
+      {links.map((link) => (
+        <NavLink key={link.name} link={link} />
+      ))}
     </NavBarContainer>
   );
 };
