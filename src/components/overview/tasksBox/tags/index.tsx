7378914a434d484351cec7ae565ba48d30f0a50c
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-10-03 16:30:33 +0300

    fix: Tag rearenged

--- src/components/overview/tasksBox/tags/index.tsx
+++ src/components/overview/tasksBox/tags/index.tsx
@@ -5,20 +5,23 @@ interface TagProps {
   text: string;
 }
 
-const Tag: React.FC<TagProps> = ({ text }) => {
-  let color = colors.primaryGray;
-  let backgroundColor = colors.grayLightest;
+const tagColorMap: Record<string, { color: string; backgroundColor: string }> = {
+  Urgent: {
+    color: colors.primaryWhite,
+    backgroundColor: colors.primaryYellow,
+  },
+  New: {
+    color: colors.primaryWhite,
+    backgroundColor: colors.primaryGreen,
+  },
+  Default: {
+    color: colors.primaryGray,
+    backgroundColor: colors.grayLightest,
+  },
+};
 
-  if (text === 'Urgent') {
-    color = colors.primaryWhite;
-    backgroundColor = colors.primaryYellow;
-  } else if (text === 'New') {
-    color = colors.primaryWhite;
-    backgroundColor = colors.primaryGreen;
-  } else if (text === 'Default') {
-    color = colors.primaryGray;
-    backgroundColor = colors.grayLightest;
-  }
+const Tag: React.FC<TagProps> = ({ text }) => {
+  const { color, backgroundColor } = tagColorMap[text] || tagColorMap.Default;
 
   return (
     <StyledTag color={color} backgroundColor={backgroundColor}>
