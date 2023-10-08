a8b1b13778113aa5f81483fa0e3eadd6679f7bde
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-10-09 02:46:31 +0300

    [feat(fix): T-8]:  extraReducers => reducers

--- src/store/tasks/slice.ts
+++ src/store/tasks/slice.ts
@@ -2,7 +2,6 @@ import { tasks } from '../../utils/mockData';
 import { v4 as uuidv4 } from 'uuid';
 import { IAddTaskPayload, ITasksState, IToggleTaskPayload } from '../../types/tasks';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { addTask, toggleTask } from './actions';
 
 const initialState: ITasksState = {
   tasks,
@@ -11,22 +10,21 @@ const initialState: ITasksState = {
 export const tasksSlice = createSlice({
   name: 'tasks',
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(addTask, (state, action: PayloadAction<IAddTaskPayload>) => {
+  reducers: {
+    addTask: (state, action: PayloadAction<IAddTaskPayload>) => {
       state.tasks.push({
         text: action.payload.text,
         tag: action.payload.tag,
         checked: false,
         id: uuidv4(),
       });
-    });
-    builder.addCase(toggleTask, (state, action: PayloadAction<IToggleTaskPayload>) => {
+    },
+    toggleTask: (state, action: PayloadAction<IToggleTaskPayload>) => {
       const task = state.tasks.find((task) => task.id === action.payload.id);
       if (task) {
         task.checked = !task.checked;
       }
-    });
+    },
   },
 });
 
