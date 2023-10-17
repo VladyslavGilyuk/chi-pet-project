d94a8f8f699ca3622fdb0ee1a81c403c62ff1763
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-10-17 17:01:45 +0300

    [(fix): T-10]:  Rows & Columns in helper.ts

--- src/components/tickets/styled.ts
+++ src/components/tickets/styled.ts
@@ -1,6 +1,16 @@
 import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import styled from 'styled-components';
 
+const StyledDataGrid = styled(DataGrid)`
+  && {
+    & .MuiDataGrid-row,
+    & .MuiDataGrid-columnHeader {
+      padding-left: 32px;
+      padding-right: 32px;
+    }
+  }
+`;
 const StyledStack = styled(Box)`
   display: flex;
   gap: 24px;
@@ -12,4 +22,4 @@ const UserPhoto = styled.img`
   object-fit: cover;
 `;
 
-export { UserPhoto, StyledStack };
+export { UserPhoto, StyledStack, StyledDataGrid };
--- src/utils/dateFunctions.ts
+++ src/utils/dateFunctions.ts
@@ -4,3 +4,16 @@ import { format } from 'date-fns';
 export const formatAsOfDate = (currentDate: Date) => {
   return format(currentDate, "'as of' dd MMM yyyy, hh:mm a");
 };
+export const formatAsCreateDate = (currentDate: Date) => {
+  return format(currentDate, "'on' dd.MM.yyyy");
+};
+export const formatAsDedlineDate = (currentDate: Date) => {
+  return format(currentDate, 'MMMM d, yyyy');
+};
+export const formatAsHoursDate = (currentDate: Date) => {
+  return format(currentDate, 'h:mm a');
+};
+export const updatedDifference = (updatedDate: Date) => {
+  const timeDifference = Math.abs(new Date().getTime() - updatedDate.getTime());
+  return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+};
