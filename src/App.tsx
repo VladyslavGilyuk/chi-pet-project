c2eab0ea52c9cc511ea4fccd7f49d141e32965d6
VladyslavGilyuk <vladgilyuk@gmail.com>
2023-12-01 17:03:02 +0200

    [(feat): T-16]: Lazy Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-import Contacts from './pages/Contacts';
+import CircularSpinner from './components/common/spinners/circular';
 import ErrorBoundary from './components/errorBoundary';
-import Layout from './layouts/mainLayout';
-import Overview from './pages/Overview';
-import PageNotFound from './pages/NotFound';
-import PrivateRoute from './routes/privateRoute';
-import PublicRoute from './routes/publicRoute';
 import { ROUTE_PATH } from './routes';
-import SignInPage from './pages/SignIn';
-import SignUpPage from './pages/SignUp';
 import { ThemeProvider } from '@mui/material';
-import Tickets from './pages/Tickets';
 import { GlobalStyles, theme } from './styled';
 import { Route, Routes } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+
+const LazyContacts = lazy(() => import('./pages/Contacts'));
+const LazyOverview = lazy(() => import('./pages/Overview'));
+const LazyPageNotFound = lazy(() => import('./pages/NotFound'));
+const LazySignInPage = lazy(() => import('./pages/SignIn'));
+const LazySignUpPage = lazy(() => import('./pages/SignUp'));
+const LazyLayout = lazy(() => import('./layouts/mainLayout'));
+const LazyTickets = lazy(() => import('./pages/Tickets'));
+const LazyPrivateRoute = lazy(() => import('./routes/privateRoute'));
+const LazyPublicRoute = lazy(() => import('./routes/publicRoute'));
 
 function App() {
   return (
     <ErrorBoundary>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <Routes>
-          <Route element={<PublicRoute />}>
-            <Route path={ROUTE_PATH.SignIn} index element={<SignInPage />} />
-            <Route path={ROUTE_PATH.SignUp} element={<SignUpPage />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path={ROUTE_PATH.Home} element={<Layout />}>
-              <Route index path={ROUTE_PATH.Home} element={<Overview />} />
-              <Route path={ROUTE_PATH.Tickets} element={<Tickets />} />
-              <Route path={ROUTE_PATH.Contacts} element={<Contacts />} />
+        <Suspense fallback={<CircularSpinner />}>
+          <Routes>
+            <Route element={<LazyPublicRoute />}>
+              <Route path={ROUTE_PATH.SignIn} index element={<LazySignInPage />} />
+              <Route path={ROUTE_PATH.SignUp} element={<LazySignUpPage />} />
             </Route>
-          </Route>
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
+            <Route element={<LazyPrivateRoute />}>
+              <Route path={ROUTE_PATH.Home} element={<LazyLayout />}>
+                <Route index path={ROUTE_PATH.Home} element={<LazyOverview />} />
+                <Route path={ROUTE_PATH.Tickets} element={<LazyTickets />} />
+                <Route path={ROUTE_PATH.Contacts} element={<LazyContacts />} />
+              </Route>
+            </Route>
+            <Route path='*' element={<LazyPageNotFound />} />
+          </Routes>
+        </Suspense>
       </ThemeProvider>
     </ErrorBoundary>
   );
---
+++ src/components/common/spinners/circular/index.tsx
@@ -0,0 +1,12 @@
+import { CircularProgress } from '@mui/material';
+import { StyledBox } from './styled';
+
+function CircularSpinner() {
+  return (
+    <StyledBox>
+      <CircularProgress />
+    </StyledBox>
+  );
+}
+
+export default CircularSpinner;
